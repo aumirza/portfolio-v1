ec2ae29b8ca11fd296b5f15669d01d1950eb1d59
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2024-11-13 18:12:19 +0530

    fix: lint errors

--- src/Components/UI/ProgressBar/ProgressBar.js
+++ src/Components/UI/ProgressBar/ProgressBar.js
@@ -2,7 +2,7 @@ export const ProgressBar = ({ label, value, color, width }) => {
 
     return (
 
-        <div className={"my-1" + " " + (width ? 'w-' + width : 'w-56')}>
+        <div className={"my-1 " + (width ? 'w-' + width : 'w-56')}>
             <div className="bg-gray-200 h-5 rounded-full">
                 <div className="flex h-5 items-center justify-center text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full bg-blue-600"
                     style={{ width: value + '%' }} > {value}%</div>
